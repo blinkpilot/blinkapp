7b4af1ee4a8b92a94f4f14b1d57ffdbfeb0b11b2
Olga Sizova <olisizova@gmail.com>
2016-11-24 15:05:52 -0500

    first cut of react app

--- README.md
+++
@@ -1,2 +0,0 @@
-# blinkapp
-React App
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './App'
+import './styles/app.css'
+
+render(<App/>, document.getElementById('main'))
